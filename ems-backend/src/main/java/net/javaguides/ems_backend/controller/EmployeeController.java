c8abbec94ce12d898bc7177efa45de7f14e2c71f
asirialwis <asirichathurika900@gmail.com>
2024-10-25 17:39:52 +0530

    add employee functionalities

---
+++ ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package net.javaguides.ems_backend.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto>createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee =  employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
