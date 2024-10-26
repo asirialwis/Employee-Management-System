5c43f6c847219444edec3f5fe2650d91efa38cdb
asirialwis <asirichathurika900@gmail.com>
2024-10-26 12:18:26 +0530

    add log infor for post request

--- ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -6,17 +6,23 @@ import net.javaguides.ems_backend.entity.Employee;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
 import net.javaguides.ems_backend.repository.EmployeeRepository;
 import net.javaguides.ems_backend.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static final Logger log = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepository employeeRepository;     //Dependency Inject
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee =  employeeRepository.save(employee);
+
+        log.info("Employee saved: {}", savedEmployee);
+
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 }
